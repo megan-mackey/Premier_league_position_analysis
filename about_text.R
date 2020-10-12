cfc9f2a917c77d3e4e337bb92087d6039c2dda09
Megan Mackey <megan.mackey@megans-mbp.home>
2020-10-12 15:45:01 +0100

    Processed Data

---
+++ about_text.R
@@ -0,0 +1,13 @@
+# this file contains text to load into the about page, in an effort to not
+# over-clutter by app.R file with text
+
+p1 <- "My project is going to look at the economic consequences of the position you finish in the English
+               premier league. I have found a csv file online which shows the position of each premier league from 2005 to 2015.
+               So I am planning to filter that down to 2010-2015 so it is not too much data. I would then compare
+               this with the market value of premier league teams during this period. There is also the possibility to compare the premier league
+               to the other 4 major leagues in Europe and see how their revenues relate. Also,I could see how the economic position of teams affects how much they spend on transfers and we can also look at top
+               10 transfers. The first two are the major points I want to go off and the other two could be supplementary."
+p2 <- "My name is Megan and I study Government. 
+             You can reach me at mmackey@college.harvard.edu. Here is the link to
+               my repo:
+               https://github.com/megan-mackey/Final_project"
