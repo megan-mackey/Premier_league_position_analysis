7c9ee7602d5ed3349d3f041bdb34ae0d9a508fbe
Megan Mackey <megan.mackey@Megans-MacBook-Pro.local>
2020-11-18 14:46:38 +0000

    Creator information updated

--- Shiny App/creator_information.R
+++ Shiny App/creator_information.R
@@ -4,6 +4,17 @@
 p1 <- "This is my final project for Gov 50: Data. I originally came up with the idea to look at the finances of the Premier league because I am a avid soccer fan and particularly in the last year with the COVID-19 pandemic, it really intrigued me how past seasons might help teams with the finances today. 
 The Premier League is the most watched league across the world and the position you finish in is highly important to the amount of monday you recieve. To learn more about the process, please look at the premier league tab"
 
+p2 <- "Ultimately, the main takeaway from this analysis of the premier league is that every league position matters. Whether this is fighting at the top of the table or the bottom of the table, the margins are huge.
+From the model, we can see that total payment increases as the league position decreases. There is a negative correlation. The data explains why so many teams who have just come up from the championship struggle to stay in
+the premier league, because the other teams have the financial help of the league from the previous season. This means they can spend more money on salaries, facilities and transfers."
+
+p3<- " One of the consequences looked at from receiving the money for transfer and specifically the highest possible transfer of a player. A transfer is when money is exchanged for a player and we are looking at the highest number the club has paid during the model.
+As shown, there is evidence that position and highest transfer fee are linked and that I am not drawing at strings. So the conclusions drawn show that the money received is the affecting the amount of money spent on transfers."
+
+p4 <- "For the teams that are finishing in the top 6 , the money element is not something they worry about but the lower down the table the more they worry about the income and the consequences are obviously as they cannot spend on the team. The key for teams
+is to break the cycle and move up the table because the money received even for a place higher is so important !!"
+
+
 p8 <- "My name is Megan Mackey and I'm a member of Harvard's class of 2023 with a BA in Government. You can reach me at 
 megan.mackey01@gmail.com and find my repository at "
 
